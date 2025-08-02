b6a304fe0a61d2f055050a618e6a00a0adc75d4f
James <js@sugrono.com>
2025-08-02 17:22:07 +1000

    force initial

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,10 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
+import robots from 'astro-robots';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+    site: 'https://jimsug.github.io',
+    integrations: [mdx(), sitemap(), robots()],
 });
